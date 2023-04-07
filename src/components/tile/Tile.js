c363ea103339b6fd7c9eecb7b72b0cac173a6acf
Eingest <ak0805@live.de>
2023-04-07 16:03:57 +0200

    added Tile functionality

--- src/components/tile/Tile.js
+++ src/components/tile/Tile.js
@@ -1,9 +1,15 @@
 import React from "react";
 
-export const Tile = () => {
+export const Tile = ({ data }) => {
   return (
-    <div className="tile-container">
-      
+    <div className="tile-container" key={data.title}>
+      {Object.keys(data).map((key) =>
+        key === "title" || key === "name" ? (
+          <p className="tile-title">{data.title || data.name}</p>
+        ) : (
+          <p className="tile">{data[key]}</p>
+        )
+      )}
     </div>
   );
 };
